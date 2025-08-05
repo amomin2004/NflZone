7e73fbd2feac5f626c7b0030ed1486c28c28e5ad
amomin2004 <alimomin2004@yahoo.com>
2025-08-04 22:07:55 -0400

    SpringBoot Set Up + Connection to postgresql

---
+++ backend/src/test/java/com/fantasyNFL/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasyNFL.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
